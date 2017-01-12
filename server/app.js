a2b53cb3dfcb1299b25fa92e386e387a37811dd8
Rich Parrish <richparrishx@gmail.com>
2017-01-12 10:34:47 -0800

    try admin

--- server/app.js
+++ server/app.js
@@ -8,13 +8,13 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../Livemuni2-196985dacc4a.json");
 
 var lm = {
-  fb: firebase.initializeApp({
-    apiKey: config.apiKey,
-    authDomain: config.authDomain,
-    databaseURL: config.databaseURL,
-    storageBucket: config.storageBucket,
-    messagingSenderId: config.messagingSenderId
-  }),
+  // fb: firebase.initializeApp({
+  //   apiKey: config.apiKey,
+  //   authDomain: config.authDomain,
+  //   databaseURL: config.databaseURL,
+  //   storageBucket: config.storageBucket,
+  //   messagingSenderId: config.messagingSenderId
+  // }),
   formatJSON: function(body) { 
     var responseJSON = parser.toJson(body, {object: true});
     var vehicleObj = responseJSON.body.vehicle; // array-like object with index keys
@@ -77,8 +77,15 @@ var lm = {
   saveToFirebase: function(result) {
     console.log('saving to firebase');
 
-    lm.fb.database().ref('/node').set(result).then(function() {
+    var fb = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: config.databaseURL
+    });
+
+    fb.database().ref('/node').set(result).then(function(a,b,c) {
       console.log('saved!');
+      console.log(a);
+      console.log(b);
     });
   },
 };
@@ -91,11 +98,6 @@ http.createServer(function(req, res) {
   });
   res.on('error', function(err) {
     console.error(err);
-  });
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: config.databaseURL
   });
 
   setInterval(lm.getAndWriteData, 10000);
